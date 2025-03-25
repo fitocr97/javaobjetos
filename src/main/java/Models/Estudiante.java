feb8d95abff3723f001452a322e85b4e7eff94f7
Rodolfo Mora <rodolfomora97@outlook.com>
2025-03-25 14:59:57 -0600

    se creo la calse estudiante

--- src/main/java/Models/Estudiante.java
+++ src/main/java/Models/Estudiante.java
@@ -9,5 +9,71 @@ package Models;
  * @author Rodolfo
  */
 public class Estudiante {
+    String nombre;
+    int proyecto1;
+    int examen1;
+    int examen2;
+    int proyecto2;
+    
+    public Estudiante(){
+        
+    }
+    
+    public Estudiante(String nombre, int proyecto1, int examen1, int examen2, int proyecto2){
+        this.nombre = nombre;
+        this.examen1 = examen1;
+        this.examen2 = examen2;
+        this.proyecto2 = proyecto2;
+        
+    }
+
+    public int getProyecto1() {
+        return proyecto1;
+    }
+
+    public int getExamen1() {
+        return examen1;
+    }
+
+    public int getExamen2() {
+        return examen2;
+    }
+
+    public int getProyecto2() {
+        return proyecto2;
+    }
+
+    public void setProyecto1(int proyecto1) {
+        this.proyecto1 = proyecto1;
+    }
+
+    public void setExamen1(int examen1) {
+        this.examen1 = examen1;
+    }
+
+    public void setExamen2(int examen2) {
+        this.examen2 = examen2;
+    }
+
+    public void setProyecto2(int proyecto2) {
+        this.proyecto2 = proyecto2;
+    }
+
+
+    
+     public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //funciones metodos
+
+   public double notaTotal(){
+       return getExamen1()+ getExamen2() + getProyecto1() + getProyecto2();
+   }
+    
     
 }
